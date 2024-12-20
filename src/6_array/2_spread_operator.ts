b4ed09a5935e67e238e4c6ec9299d7c8efa18819
김선우 <frod5@kakao.com>
2024-12-20 15:56:19 +0900

    타입스크립트 스터디 - spread operator (...)

---
+++ src/6_array/2_spread_operator.ts
@@ -0,0 +1,14 @@
+/**
+ * Spread Operator
+ */
+const onlyString = ['1', '2', '3'];
+const onlyNumbers = [1, 2, 3, 4, 5];
+
+const arr1 = [
+    ...onlyString
+]
+
+const arr2 = [
+    ...onlyString,
+    ...onlyNumbers
+]
