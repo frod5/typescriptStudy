ae83d3cf06e2c2357918e9ad580b2788e83aa16a
김선우 <frod5@kakao.com>
2024-12-30 17:16:52 +0900

    타입스크립트 스터디 - parameter type

---
+++ src/11_utility_type/9_parameter_type.ts
@@ -0,0 +1,10 @@
+/**
+ * parameter type
+ */
+
+const sampleFunction = (x:number, y:string, z:boolean) => {
+
+}
+
+type Params = Parameters<typeof sampleFunction>;
+type Params2 = Parameters<(one:number) => void>;
